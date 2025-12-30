31358efe05a25f26986eeddd0a362abd0728aa87
furpalama2062-eng <furpalama2062@gmail.com>
2025-12-30 13:28:44 +0545

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Student Management System");
-	}
+		System.out.println("Welcome to Student Management System:\n\n");
+		
+		Student s = new Student("Ram", 20);
+		GradeCalculator g = new GradeCalculator();
 
+	        double avg = g.calculateAverage(85, 90, 78);
+	        String grade = g.getLetterGrade(avg);
+	        g.displayGrade(s.getName(), avg, grade);
+			}
 }
