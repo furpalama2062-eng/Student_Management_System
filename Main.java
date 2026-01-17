7c749e6fb62f199a9aee4cb2b9d6a6da3dccf688
furpalama2062-eng <furpalama2062@gmail.com>
2026-01-17 20:43:53 +0545

    Initial commit with main classs

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Student Management System:\n\n");
-		
-		Student s = new Student("Ram", 20);
-		GradeCalculator g = new GradeCalculator();
+		System.out.println("Student management System started");
+	}
 
-	        double avg = g.calculateAverage(85, 90, 78);
-	        String grade = g.getLetterGrade(avg);
-	        g.displayGrade(s.getName(), avg, grade);
-			}
 }
